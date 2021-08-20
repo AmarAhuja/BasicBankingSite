1138d50c1c571d70a29fed32bbe55185bda97361
AmarAhuja <70715279+AmarAhuja@users.noreply.github.com>
2021-08-21 03:01:10 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php 
+
+$server="localhost:3307";
+$username="root";
+$db="sparks_bank";
+$password="";
+
+$conn=mysqli_connect($server,$username,$password,$db);
+
+if($conn){
+  
+}
+
+else
+die("connection to this database failed due to " .mysqli_connect_error());
+
+
+?>
+
+
